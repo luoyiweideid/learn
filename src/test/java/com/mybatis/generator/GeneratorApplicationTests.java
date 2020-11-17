ea15cffff2dd73cee015c867b682119220d51e63
rain <951285710@qq.com>
2020-11-17 16:30:34 +0800

    【+】- 初始化Spring Boot + MyBatis Generator 反向工程

---
+++ src/test/java/com/mybatis/generator/GeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatis.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
