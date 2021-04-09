09ce94913032bb497ae434c7b43fcbb7b5726a58
luoyiwei <951285710@qq.com>
2021-04-09 15:50:36 +0800

    【fix】- 测试修改

--- src/main/java/com/cn/learn/test/Test01.java
+++ src/main/java/com/cn/learn/test/Test01.java
@@ -11,10 +11,10 @@ public class Test01 {
 
     public static void main(String[] args) {
         Test test = new Test();
-        //test.setDesc("1223");
-        //test.setPhone("2343");
-        //test.setTestId(1);
+        test.setDesc("1223");
         String value = JSON.toJSONString(test);
+        System.out.println(value);
+        //测试修改 - 分支冲突
         System.out.println(value);
     }
 }
